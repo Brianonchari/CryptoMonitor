4658eae3de02809f4cdfcd3b4e6f5f557823aede
Brianonchari <oncharibrian@gmail.com>
2019-02-16 12:46:54 +0300

    Android app to monitor BTC and ETH

---
+++ app/src/main/java/com/example/cryptomonitor/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.cryptomonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView bitcoinTv, etheriumTv,textViewCurrency;
+    private static String URL_DATA = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR";
+    private RecyclerView recyclerView;
+    private List<CardItems> cardItemsList;
+    private RecyclerView.Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //init Toolbar
+        Toolbar toolbar = findViewById(R.id.toolBar);
+         setSupportActionBar(null);
+         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //init TextView objects
+
+        bitcoinTv = findViewById(R.id.bitcoinTv);
+        etheriumTv = findViewById(R.id.etheriumTv);
+        textViewCurrency = findViewById(R.id.textViewCurrency);
+
+        //init recyclerView
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        cardItemsList = new ArrayList<>();
+
+
+        loadURLData();
+        //TODO set adapter
+
+    }
+
+    public void loadURLData(){
+        /**
+         * This class makes and api call
+         */
+        JsonObjectRequest DataRequest = new JsonObjectRequest(Request.Method.GET, URL_DATA, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject btc_values = response.getJSONObject("BTC".trim());
+                            JSONObject eth_values = response.getJSONObject("ETH".trim());
+
+                            Iterator<?> keysBTC = btc_values.keys();
+                            Iterator<?> keysETH = eth_values.keys();
+
+
+                            while (keysBTC.hasNext() && keysETH.hasNext()) {
+                                String keyBTC = (String) keysBTC.next();
+                                String keyETH = (String) keysETH.next();
+
+                                CardItems card = new CardItems(keyBTC, btc_values.getDouble(keyBTC), eth_values.getDouble(keyETH));
+                                cardItemsList.add(card);
+                            }
+
+                            adapter = new MyAdapter(cardItemsList, getApplicationContext());
+                            recyclerView.setAdapter(adapter);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Error" + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(DataRequest);
+
+    }
+}
---
+++ app/src/main/java/com/example/cryptomonitor/MyAdapter.java
@@ -0,0 +1,68 @@
+package com.example.cryptomonitor;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+    public MyAdapter(List<CardItems> cardItemsList, Context context) {
+
+        this.cardItemsList = cardItemsList;
+        this.context = context;
+    }
+
+    private List<CardItems> cardItemsList;
+    private Context context;
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_items, parent, false);
+
+        return new ViewHolder(v);
+
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+
+
+        CardItems cardItem = cardItemsList.get(position);
+
+        final String curr = cardItem.getCurrency();
+        final double btcVal = cardItem.getBtc_value();
+        final double EthVal = cardItem.getEth_value();
+
+        holder.currency.setText(curr);
+        holder.btc_value.setText(String.format("%1$,.2f",btcVal));
+        holder.eth_value.setText(String.format("%1$,.2f",EthVal));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return cardItemsList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView btc_value, eth_value, currency;
+        LinearLayout cardLinearLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            btc_value = itemView.findViewById(R.id.bitcoinTv);
+            eth_value = itemView.findViewById(R.id.bitcoinTv);
+            currency = itemView.findViewById(R.id.textViewCurrency);
+            cardLinearLayout = itemView.findViewById(R.id.cardLinearLayout);
+        }
+    }
+}
